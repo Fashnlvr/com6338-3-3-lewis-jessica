260e22a79e06b787d440bf99572c60b22ea74340
p <jlewis0613@outlook.com>
2025-02-02 16:34:36 -0500

    quiz

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+var questionsArr = [
+    {
+        question: 'Javascript is a programming language',
+        answer: true 
+    },
+    {
+        question: 'The Earth is flat',
+        answer: false
+    },
+    {
+        question: 'The Great Wall of China can be seen from space',
+        answer: false
+    },
+    {
+        question: 'Lightning can strike in the same place twice',
+        answer: true
+    },
+    {
+        question: 'Mount Everest is the tallest mountain in the world',
+        answer: true
+    }
+];
+
+function runQuiz {
+    var correctAnswers = 0;
+
+    for (var i=0; i < questionsArr.length; i++) {
+        var userAnswer = confirm(questionsArr[i].question);
+        if (userAnswer === questionsArr[i].answer) {
+            correctAnswers++;
+        }
+    }
+
+    var scorePercentage = Math.round((correctAnswers / questionsArr.length) * 100);
+    alert('You got ' + scorePercentage + '% correct!')
+}
