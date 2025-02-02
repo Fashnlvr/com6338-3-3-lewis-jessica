f2f4c796d7fe170c67da3894bf06dcb38c9fc8e4
p <jlewis0613@outlook.com>
2025-02-02 17:41:01 -0500

    punctuation

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 var questionsArr = [
     {
-        question: 'Javascript is a programming language',
+        question: 'Javascript is a programming language.',
         answer: true 
     },
     {
-        question: 'The Earth is flat',
+        question: 'The Earth is flat.',
         answer: false
     },
     {
-        question: 'The Great Wall of China can be seen from space',
+        question: 'The Great Wall of China can be seen from space.',
         answer: false
     },
     {
-        question: 'Lightning can strike in the same place twice',
+        question: 'Lightning can strike in the same place twice.',
         answer: true
     },
     {
-        question: 'Mount Everest is the tallest mountain in the world',
+        question: 'Mount Everest is the tallest mountain in the world.',
+        answer: true
+    },
+    {
+        question: 'World War II ended in 1945.',
         answer: true
     }
 ];
 
-function runQuiz {
+function runQuiz() {
     var correctAnswers = 0;
 
     for (var i=0; i < questionsArr.length; i++) {
